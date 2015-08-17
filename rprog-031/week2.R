f79905537d47f80538f0fbdeb061495071dc5439
Robert Impey <robert@impey.info>
2015-08-17 08:00:31 +0100

    031 wk2 ex 2

--- rprog-031/week2.R
+++ rprog-031/week2.R
@@ -1,8 +1,12 @@
+make_file_name <- function(directory, id) {
+  sprintf("%s/%.3d.csv", directory, id)
+}
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   readings <- numeric()
   
   for (i in id) {
-    file_name <- sprintf("%s/%.3d.csv", directory, i)
+    file_name <- make_file_name(directory, i)
     
     file_data <- read.csv(file_name, header = TRUE)
     
@@ -22,4 +26,39 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   }
   
   mean(readings)
+}
+
+complete <- function(directory, id = 1:332) {
+  complete_ids = numeric()
+  complete_nobs = numeric()
+  
+  for (i in id) {
+    file_name <- make_file_name(directory, i)
+    
+    num_obs = 0
+    
+    file_data <- read.csv(file_name)
+
+    nitrate <- file_data$nitrate
+    sulfate <- file_data$sulfate
+    
+    for (j in seq_along(nitrate)) {
+      
+      if (is.na(nitrate[j])) {
+        #print("nitrate is nan!")
+      } 
+      else {
+        if (is.na(sulfate[j])) {
+          #print("sulfate is nan!")
+        } else {
+          num_obs <- num_obs + 1
+        }
+      }
+    }
+    
+    complete_ids <- append(complete_ids, i)
+    complete_nobs <- append(complete_nobs, num_obs)
+  }
+  
+  data.frame(id = complete_ids, nobs = complete_nobs)
 }
