706500afddb7feb8c691ed0d0d79765f7cb46f8a
Robert Impey <robert@impey.info>
2015-08-17 08:47:50 +0100

    031 Wk2 Ex3

--- rprog-031/week2.R
+++ rprog-031/week2.R
@@ -62,3 +62,24 @@ complete <- function(directory, id = 1:332) {
   
   data.frame(id = complete_ids, nobs = complete_nobs)
 }
+
+corr <- function(directory, threshold = 0) {
+  complete_obs <- complete(directory)
+  
+  obs_above_threshold <- complete_obs[complete_obs$nobs > threshold,]
+  
+  correlations <- numeric()
+  for (i in obs_above_threshold$id) {
+    file_name <- make_file_name(directory, i)
+    
+    file_data <- read.csv(file_name, header = TRUE)
+    
+    nitrate <- file_data$nitrate
+    sulfate <- file_data$sulfate
+    
+    file_corr <- cor(nitrate, sulfate, use = 'complete.obs')
+    
+    correlations <- append(correlations, file_corr)
+  }
+  correlations
+}
